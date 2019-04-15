d1cc5e23642c331376bce01de7a9c454f1c06030
stevesung <steveysung91@gmail.com>
2019-04-15 12:38:28 +0900

    state:loading state

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -7,32 +7,27 @@ class App extends Component {
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Update : componentReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate()
   //          -> render() -> componentDidUpdate()
-  state = {
-    greeting: "hello!",
-    movies: [
-      {
-        title: "Matirx",
-        poster:
-          "http://2korea.hani.co.kr/files/attach/images/64/981/316/001.jpg"
-      },
-      {
-        title: "Avengers",
-        poster:
-          "https://upload.wikimedia.org/wikipedia/ko/thumb/9/92/%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4_%EC%9D%B8%ED%94%BC%EB%8B%88%ED%8B%B0_%EC%9B%8C.jpg/250px-%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4_%EC%9D%B8%ED%94%BC%EB%8B%88%ED%8B%B0_%EC%9B%8C.jpg"
-      },
-      {
-        title: "Captain Marvel",
-        poster:
-          "https://t1.daumcdn.net/movie/9633f50f32a34df2ae91dbac1203062e1551065947586"
-      }
-    ]
-  };
+  state = {};
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies, // 이전것들 그대로 두고
+          {
+            title: "Matirx",
+            poster:
+              "http://2korea.hani.co.kr/files/attach/images/64/981/316/001.jpg"
+          },
+          {
+            title: "Avengers",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/ko/thumb/9/92/%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4_%EC%9D%B8%ED%94%BC%EB%8B%88%ED%8B%B0_%EC%9B%8C.jpg/250px-%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4_%EC%9D%B8%ED%94%BC%EB%8B%88%ED%8B%B0_%EC%9B%8C.jpg"
+          },
+          {
+            title: "Captain Marvel",
+            poster:
+              "https://t1.daumcdn.net/movie/9633f50f32a34df2ae91dbac1203062e1551065947586"
+          },
           {
             title: "Transpotting",
             poster:
@@ -43,15 +38,18 @@ class App extends Component {
     }, 5000);
   }
 
+  _renderMovies = () => {
+    //_를 붙인이유는 리액트 자체 함수도 많아서 섞임방지
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movies;
+  };
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          // index는 우리가 제공하는 리스트의 숫자 의미 0부터 2까지 들어갈것
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
